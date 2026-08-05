86e1883bf8fe395bf184c35a1ffd6b4dbb06cf08
KuboOrk <xakker.91@list.ru>
2026-08-05 17:55:11 +0300

    Fixed path to the lib

--- PYUSD/contracts/lib/EIP2612.sol
+++ PYUSD/contracts/lib/EIP2612.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.17;
 import { PaxosBaseAbstract } from "./PaxosBaseAbstract.sol";
 import { EIP712Domain } from "./EIP712Domain.sol";
 import { EIP2612Definitions } from "./EIP2612Definitions.sol";
-import { SignatureChecker } from "PYUSD/@openzeppelin/contracts/utils/cryptography/SignatureChecker.sol";
+import { SignatureChecker } from "../../@openzeppelin/contracts/utils/cryptography/SignatureChecker.sol";
 
 /**
  * @title EIP2612 contract
